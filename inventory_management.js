2bcc3e8ad41fc279ea8491e48cf99c09add5e813
Lea Delic <leadelic0429@gmail.com>
2024-09-15 16:10:23 -0400

    "Initialize inventory with product objects"

---
+++ inventory_management.js
@@ -0,0 +1,10 @@
+//task 1
+const inventory= [
+    {name:'Laptop', price: 1200, quantity: 10, lowStocklevel: 3},
+    {name:'Smartphone', price: 800, quantity: 5, lowStocklevel: 2},
+    {name:'Tablet', price: 400, quantity: 5, lowStocklevel: 1},
+    {name: ' headphones', price:100, quantity: 15, lowStocklevel:5},
+    {name:'smartwatch', price:250, quantity:3, lowStocklevel:1}
+];
+
+    ]
